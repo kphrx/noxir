3c2c92ad2c83e54c88f738461006cfabdb727bdc
kPherox <kphrx@kpherox.dev>
2023-11-05 16:49:25 +0900

    refactor: websocket upgrade and halt before `match/3`
    refactor: use `Plug.Head` and `plug :connect` instead of `via: [:get, :head, :connect]`

--- lib/noxir/router.ex
+++ lib/noxir/router.ex
@@ -1,24 +1,36 @@
 defmodule Noxir.Router do
   use Plug.Router
 
-  plug(Plug.Logger)
-  plug(:match)
-  plug(:dispatch)
+  plug Plug.Logger
 
-  match "/", via: [:get, :head, :connect] do
+  plug Plug.Head
+  plug :connect
+
+  plug :websocket
+
+  plug :match
+  plug :dispatch
+
+  get "/" do
+    send_resp(conn, 200, """
+    Please use a Nostr client to connect.
+    """)
+  end
+
+  match _ do
+    send_resp(conn, 404, "not found")
+  end
+
+  def connect(%Plug.Conn{method: "CONNECT"} = conn, _), do: %{conn | method: "GET"}
+  def connect(conn, _), do: conn
+
+  def websocket(conn, _) do
     case WebSockAdapter.UpgradeValidation.validate_upgrade(conn) do
       :ok ->
         conn |> WebSockAdapter.upgrade(Noxir.Relay, [], timeout: 60_000) |> halt()
 
       _ ->
         conn
-        |> send_resp(200, """
-        Please use a Nostr client to connect.
-        """)
     end
-  end
-
-  match _ do
-    send_resp(conn, 404, "not found")
   end
 end
