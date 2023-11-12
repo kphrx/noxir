ab74f1629ccb88c2abc1c39669387a64af9c9f08
kPherox <kphrx@kpherox.dev>
2023-11-13 01:53:03 +0900

    fix: unnecessary delete before write update (#4)

--- lib/noxir/store/connection.ex
+++ lib/noxir/store/connection.ex
@@ -21,28 +21,22 @@ defmodule Noxir.Store.Connection do
 
   @spec subscribe(pid(), binary(), [map()]) :: Memento.Table.record() | no_return()
   def subscribe(pid, sub_id, filters) do
-    conn =
-      __MODULE__
-      |> Query.read(pid)
-      |> Map.replace_lazy(:subscriptions, fn subs ->
-        List.keystore(subs, sub_id, 0, {sub_id, filters})
-      end)
-
-    Query.delete(__MODULE__, pid)
-    Query.write(conn)
+    __MODULE__
+    |> Query.read(pid)
+    |> Map.replace_lazy(:subscriptions, fn subs ->
+      List.keystore(subs, sub_id, 0, {sub_id, filters})
+    end)
+    |> Query.write()
   end
 
   @spec close(pid(), binary()) :: Memento.Table.record() | no_return()
   def close(pid, sub_id) do
-    conn =
-      __MODULE__
-      |> Query.read(pid)
-      |> Map.replace_lazy(:subscriptions, fn subs ->
-        List.keydelete(subs, sub_id, 0)
-      end)
-
-    Query.delete(__MODULE__, pid)
-    Query.write(conn)
+    __MODULE__
+    |> Query.read(pid)
+    |> Map.replace_lazy(:subscriptions, fn subs ->
+      List.keydelete(subs, sub_id, 0)
+    end)
+    |> Query.write()
   end
 
   @spec all :: [Memento.Table.record()]
