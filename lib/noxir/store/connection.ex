db256f5b415b84afe022f0a9441ffbf8923004e5
kPherox <kphrx@kpherox.dev>
2023-11-16 16:27:39 +0900

    refactor: add struct Noxir.Store.Connection.Filter

--- lib/noxir/store/connection.ex
+++ lib/noxir/store/connection.ex
@@ -6,6 +6,28 @@ defmodule Noxir.Store.Connection do
 
   alias Memento.Query
 
+  defmodule Filter do
+    single_lowercase_letters = Enum.map(?a..?z, &to_string([&1]))
+    single_uppercase_letters = Enum.map(?A..?Z, &to_string([&1]))
+    single_letters = single_lowercase_letters ++ single_uppercase_letters
+    @tag_filters Enum.map(single_letters, &String.to_atom("##{&1}"))
+
+    @parameters [:ids, :authors, :kinds, :since, :until, :limit | @tag_filters]
+
+    attrs =
+      @parameters
+      |> Enum.map(fn
+        :kinds -> ":kinds => [integer()]"
+        attr when attr in [:since, :until, :limit] -> "#{inspect(attr)} => integer()"
+        attr -> "#{inspect(attr)} => [binary()]"
+      end)
+      |> Enum.join(",")
+
+    @type t :: unquote(Code.string_to_quoted!("%__MODULE__{#{attrs}}"))
+
+    defstruct @parameters
+  end
+
   @spec open(pid()) :: Memento.Table.record() | no_return()
   def open(pid) do
     Query.write(%__MODULE__{
@@ -19,14 +41,22 @@ defmodule Noxir.Store.Connection do
     Query.delete(__MODULE__, pid)
   end
 
-  @spec subscribe(pid(), binary(), [map()]) :: Memento.Table.record() | no_return()
-  def subscribe(pid, sub_id, filters) do
+  @spec subscribe(pid(), binary(), [Filter.t() | map()]) :: Memento.Table.record() | no_return()
+  def subscribe(pid, sub_id, [%Filter{} | _] = filters) do
     __MODULE__
     |> Query.read(pid)
     |> Map.replace_lazy(:subscriptions, fn subs ->
       List.keystore(subs, sub_id, 0, {sub_id, filters})
     end)
     |> Query.write()
+  end
+
+  def subscribe(pid, sub_id, filters) do
+    subscribe(
+      pid,
+      sub_id,
+      Enum.map(filters, &struct(Filter, Noxir.Store.change_to_existing_atom_key(&1)))
+    )
   end
 
   @spec close(pid(), binary()) :: Memento.Table.record() | no_return()
