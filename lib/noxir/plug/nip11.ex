70eb59e1ebcc238298cac553e72302129962195e
kPherox <kphrx@kpherox.dev>
2023-11-18 21:23:11 +0900

    fix: configurable for runtime

--- lib/noxir/plug/nip11.ex
+++ lib/noxir/plug/nip11.ex
@@ -14,23 +14,7 @@ defmodule Noxir.Plug.NIP11 do
   def call(conn, []) do
     case Conn.get_req_header(conn, "accept") do
       ["application/nostr+json" | _] ->
-        information = Application.fetch_env!(:noxir, :information)
-
-        conn
-        |> Conn.put_resp_content_type("application/nostr+json")
-        |> Conn.send_resp(
-          200,
-          Jason.encode!(%{
-            name: Keyword.get(information, :name, ""),
-            description: Keyword.get(information, :description, ""),
-            pubkey: Keyword.get(information, :pubkey, ""),
-            contact: Keyword.get(information, :contact, ""),
-            supported_nips: [1, 11],
-            software: Keyword.get(information, :software, ""),
-            version: "v" <> to_string(Application.spec(:noxir, :vsn))
-          })
-        )
-        |> Conn.halt()
+        Conn.put_private(conn, :nip11, true)
 
       _ ->
         conn
